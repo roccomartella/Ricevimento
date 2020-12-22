a4fe1e3ad372c40f8fa93e895ce6319cd2a5161d
Rocco Martella <essere@incontatto.link>
2020-12-22 22:24:22 +0100

    Aggiungere i file di progetto.

---
+++ Ricevimento/Global.asax.vb
@@ -0,0 +1,8 @@
+﻿
+Public Class Global_asax
+    Inherits HttpApplication
+
+    Sub Application_Start(sender As Object, e As EventArgs)
+        ' Generato all'avvio dell'applicazione
+    End Sub
+End Class
---
+++ Ricevimento/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Rivedere i valori degli attributi dell'assembly
+<Assembly: AssemblyTitle("Ricevimento")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("Ricevimento")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato per creare l'ID della libreria dei tipi
+<Assembly: Guid("0c44e727-a169-4560-a5f6-39fa0c60d833")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai quattro valori seguenti:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
+' utilizzando l'asterisco (*) come illustrato di seguito:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
